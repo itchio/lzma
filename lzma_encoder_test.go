a3bc156cda7159339f61e3bfc3793f834b93aaf1
Andrei Vieru <euvieru@gmail.com>
2010-05-07 18:38:44 +0300

    added benchmarks

--- lzma_encoder_test.go
+++ lzma_encoder_test.go
@@ -5,13 +5,16 @@
 package lzma
 
 import (
+	"bytes"
 	"io"
 	"io/ioutil"
+	"log"
+	"reflect"
 	"testing"
 )
 
 func pipe(t *testing.T, efunc func(io.WriteCloser), dfunc func(io.ReadCloser), size int64) {
-	level := 4
+	level := 3
 	pr, pw := io.Pipe()
 	go func() {
 		defer pw.Close()
@@ -86,4 +89,63 @@ func TestBoth1(t *testing.T) {
 
 func TestBoth2(t *testing.T) {
 	testBoth(t, false)
+}
+
+func TestEncoder(t *testing.T) {
+	b := new(bytes.Buffer)
+	for _, tt := range unlzmaTests {
+		if tt.err == nil {
+			pr, pw := io.Pipe()
+			defer pr.Close()
+			in := bytes.NewBuffer([]byte(tt.raw))
+			size := int64(-1)
+			if tt.size == true {
+				size = int64(len([]byte(tt.raw)))
+			}
+			go func() {
+				defer pw.Close()
+				w := NewEncoderSizeLevel(pw, size, tt.level)
+				defer w.Close()
+				_, err := io.Copy(w, in)
+				if err != nil {
+					t.Errorf("%v", err)
+				}
+			}()
+			b.Reset()
+			_, err := io.Copy(b, pr)
+			if err != nil {
+				t.Errorf("%v", err)
+			}
+			res := b.Bytes()
+			if reflect.DeepEqual(res, tt.lzma) == false {
+				t.Errorf("%s: got %d-byte %q, want %d-byte %q", tt.descr, len(res), string(res), len(tt.lzma), string(tt.lzma))
+			}
+		}
+	}
+}
+
+func BenchmarkEncoder(b *testing.B) {
+	buf := new(bytes.Buffer)
+	for i := 0; i < b.N; i++ {
+		in := bytes.NewBuffer([]byte(bk.raw))
+		pr, pw := io.Pipe()
+		go func() {
+			w := NewEncoderLevel(pw, bk.level)
+			_, err := io.Copy(w, in)
+			if err != nil {
+				log.Exitf("%v", err)
+			}
+			defer pw.Close()
+			defer w.Close()
+		}()
+		buf.Reset()
+		_, err := io.Copy(buf, pr)
+		if err != nil {
+			log.Exitf("%v", err)
+		}
+		defer pr.Close()
+	}
+	if reflect.DeepEqual(buf.Bytes(), bk.lzma) == false { // check only once, not at every iteration
+		log.Exitf("%s: got %d-byte %q, want %d-byte %q", bk.descr, len(buf.Bytes()), buf.String(), len(bk.lzma), string(bk.lzma))
+	}
 }
